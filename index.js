15786c485e334f438329c316269d1d3dbdd59eaa
mohadmin25 <abuhamar008@gmail.com>
2022-08-23 14:38:36 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+// function findMatching(name, drivers){
+//     return name.filter(
+//         (matchingNames) => matchingNames.toLowerCase() === drivers.toUpperCase()
+//     );
+// }
+
+function findMatching(name, drivers) {
+    return name.filter(
+      (matchingNames) => matchingNames.toLowerCase() === drivers.toLowerCase()
+    );
+}
+
+function fuzzyMatch(name, firstLatter){
+    return name.filter(
+        matchingNames => matchingNames.toLowerCase().indexOf(firstLatter.toLowerCase()) === 0
+    )
+}
+
+function matchName (person, propertiesByName) {
+    return person.filter(
+        details => details.name === propertiesByName
+    )
+}
